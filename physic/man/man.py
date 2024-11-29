1b62240249dd492ee3fed3aad95b181d5e695f37
liuxiaojie <1510590884@qq.com>
2024-11-29 17:50:06 +0800

    fix bug

--- physic/man/man.py
+++ physic/man/man.py
@@ -1,7 +1,7 @@
 
-import numpy as np
-
+from manim import Scene
 from manim.typing import Vector3D
+from typing_extensions import runtime
 
 from physic.man.leg import Leg
 from physic.physic_mobject import *
@@ -23,15 +23,20 @@ class Man(object):
         self.arm_length = 1 * ratio
         self.ratio = ratio
         self.bodies = []
-        self.head = PhysicalCircle(radius=self.head_radius, color=BLUE, fill_opacity=0.7)
-        self.left_eye = Circle(radius=self.eye_radius, color=WHITE, fill_opacity=1).move_to(
+        self.head = PhysicalCircle(radius=self.head_radius, color=BLUE, fill_opacity=1)
+
+        self.left_eye_ball = Circle(radius=self.eye_radius, color=WHITE, fill_opacity=1).move_to(
             self.head.get_center() + LEFT * 0.4 * ratio + UP * 0.4 * ratio)
-        self.right_eye = Circle(radius=self.eye_radius, color=WHITE, fill_opacity=1).move_to(
-            self.head.get_center() + RIGHT * 0.4 * ratio + UP * 0.4 * ratio)
         self.left_pupil = Circle(radius=self.pupil_radius, color=BLACK, fill_opacity=1).move_to(
-            self.left_eye.get_center() + LEFT * 0.05 * ratio)
+            self.left_eye_ball.get_center() + LEFT * 0.05 * ratio)
+        self.left_eye = VGroup(self.left_eye_ball, self.left_pupil)
+
+        self.right_eye_ball = Circle(radius=self.eye_radius, color=WHITE, fill_opacity=1).move_to(
+            self.head.get_center() + RIGHT * 0.4 * ratio + UP * 0.4 * ratio)
         self.right_pupil = Circle(radius=self.pupil_radius, color=BLACK, fill_opacity=1).move_to(
-            self.right_eye.get_center() + RIGHT * 0.05 * ratio)
+            self.right_eye_ball.get_center() + RIGHT * 0.05 * ratio)
+        self.right_eye = VGroup(self.right_eye_ball, self.right_pupil)
+
         self.mouth = Line(start=LEFT * 0.4 * ratio, end=RIGHT * 0.4 * ratio, color=BLACK).move_to(
             self.head.get_center() + DOWN * 0.2 * ratio)
         self.smile_mouth = Arc(radius=0.4 * ratio, start_angle=-PI * 2 / 3, angle=PI / 2, color=BLACK)
@@ -40,13 +45,21 @@ class Man(object):
                                      corner_radius=0.3*ratio).move_to(
             self.head.get_center() + DOWN * (self.head_radius + self.body_height / 2 + 0.05))
 
+        left_arm_start = self.body.get_corner(UL) + 0.1 * ratio * LEFT
+        left_arm_end = left_arm_start + np.array([
+            self.arm_length * np.cos((-1/2 -1/6) * np.pi),
+            self.arm_length * np.sin((-1/2 -1/6) * np.pi),
+            0
+        ])
+        self.left_arm = Limb(start=left_arm_start, end=left_arm_end, is_left=True, color=BLUE)
 
-        self.left_arm = Limb(start=self.body.get_corner(UL) + 0.6 * ratio * LEFT,
-                             end= self.body.get_corner(UL) + 1.2 * ratio * LEFT  + DOWN * 0.5 * ratio,
-                             is_left=True, color=BLUE)
-        self.right_arm = Limb(start=self.body.get_corner(UR) + 0.6 * ratio * RIGHT,
-                             end= self.body.get_corner(UR) + 1.2 * ratio * RIGHT  + DOWN * 0.5 * ratio,
-                             is_left=True, color=BLUE)
+        right_arm_start = self.body.get_corner(UR) + 0.1 * ratio * RIGHT
+        right_arm_end = right_arm_start + np.array([
+            self.arm_length * np.cos((-1 / 2 + 1 / 6) * np.pi),
+            self.arm_length * np.sin((-1 / 2 + 1 / 6) * np.pi),
+            0
+        ])
+        self.right_arm = Limb(start=right_arm_start, end=right_arm_end, is_left=True, color=BLUE)
         self.left_leg = Leg(start=self.body.get_corner(DL) + RIGHT * 0.1 * ratio, thigh_length=self.thigh_length,
                             shin_length=self.shin_length,
                             right=True, color=BLUE)
@@ -54,8 +67,8 @@ class Man(object):
                              shin_length=self.shin_length,
                              right=False, color=BLUE)
 
-        self.me = VGroup(self.head, self.left_eye, self.right_eye, self.left_pupil, self.right_pupil, self.mouth,
-                         self.body, self.left_arm, self.right_arm, self.left_leg.it(), self.right_leg.it())
+        self.me = VGroup(self.head, self.left_eye, self.right_eye, self.mouth, self.body,
+                         self.left_arm, self.right_arm, self.left_leg.it(), self.right_leg.it())
         drop = self.me.get_bottom() - ground_height
         if drop[1] != 0:
             self.me.move_to(self.me.get_center() + DOWN * drop[1])
@@ -68,20 +81,41 @@ class Man(object):
         for body in self.right_leg.bodies:
             self.bodies.append(body)
 
-    def face_front(self):
-        self.left_eye.move_to(
-            self.head.get_center() + LEFT * 0.4 * self.ratio + UP * 0.4 * self.ratio)
-        self.right_eye.move_to(
-            self.head.get_center() + RIGHT * 0.4 * self.ratio + UP * 0.4 * self.ratio)
-        self.left_pupil.move_to(
-            self.left_eye.get_center() + LEFT * 0.05 * self.ratio)
-        self.right_pupil.move_to(
-            self.right_eye.get_center() + RIGHT * 0.05 * self.ratio)
-        self.mouth = Line(start=LEFT * 0.4 * ratio, end=RIGHT * 0.4 * ratio, color=BLACK).move_to(
-            self.head.get_center() + DOWN * 0.2 * ratio)
+    def face_front(self, s: Scene):
+        left_arm_start = self.body.get_corner(UL) + 0.1 * self.ratio * LEFT
+        left_arm_end = left_arm_start + np.array([
+            self.arm_length * np.cos((-1 / 2 - 1 / 6) * np.pi),
+            self.arm_length * np.sin((-1 / 2 - 1 / 6) * np.pi),
+            0
+        ])
+        right_arm_start = self.body.get_corner(UR) + 0.1 * self.ratio * RIGHT
+        right_arm_end = right_arm_start + np.array([
+            self.arm_length * np.cos((-1 / 2 + 1 / 6) * np.pi),
+            self.arm_length * np.sin((-1 / 2 + 1 / 6) * np.pi),
+            0
+        ])
+        s.bring_to_back(self.head)
+        s.play(
+        self.left_pupil.animate.move_to(
+            self.left_eye_ball.get_center() + LEFT * 0.05 * self.ratio),
+            self.right_pupil.animate.move_to(
+            self.right_eye_ball.get_center() + RIGHT * 0.05 * self.ratio),
+            runtime=0.02
+        )
+        s.play(
+            self.left_eye.animate.move_to(
+                    self.head.get_center() + LEFT * 0.4 * self.ratio + UP * 0.4 * self.ratio),
+            self.right_eye.animate.move_to(
+                self.head.get_center() + RIGHT * 0.4 * self.ratio + UP * 0.4 * self.ratio),
+            self.mouth.animate.put_start_and_end_on(start=self.head.get_center() + (DOWN * 0.2 + LEFT * 0.4) * self.ratio,
+                                        end=self.head.get_center() + (DOWN * 0.2 + RIGHT * 0.4) * self.ratio),
+            self.left_arm.animate.put_start_and_end_on(start=left_arm_start, end=left_arm_end),
+            self.right_arm.animate.put_start_and_end_on(start=right_arm_start, end=right_arm_end),
+            runtime=0.05
+        )
 
-    def face_back(self):
-        return
+    def face_back(self, s: Scene):
+        s.bring_to_front(self.head)
 
     def turn(self, direction: Vector3D):
         if direction is not RIGHT and direction is not LEFT:
@@ -92,9 +126,9 @@ class Man(object):
             self.head.get_center() + direction * 0.4 * self.ratio + UP * 0.4 * self.ratio)
 
         self.left_pupil.move_to(
-            self.left_eye.get_center() + direction * 0.05 * self.ratio)
+            self.left_eye_ball.get_center() + direction * 0.05 * self.ratio)
         self.right_pupil.move_to(
-            self.right_eye.get_center() + direction * 0.05 * self.ratio)
+            self.right_eye_ball.get_center() + direction * 0.05 * self.ratio)
 
         start_point = self.head.get_center() + DOWN * 0.2 * self.ratio + direction * 0.4 * self.ratio
         x0, y0, z0 = start_point
@@ -108,6 +142,16 @@ class Man(object):
             end_x = cx - np.sqrt(a)
         end_point = np.array([end_x, y0, 0])
         self.mouth.put_start_and_end_on(start_point, end_point)
+        self.left_arm.put_start_and_end_on(start=self.body.get_corner(UP), end=self.body.get_corner(UP) + np.array([
+            self.arm_length * np.cos((-1 / 2) * np.pi),
+            self.arm_length * np.sin((-1 / 2) * np.pi),
+            0
+        ]))
+        self.right_arm.put_start_and_end_on(start=self.body.get_corner(UP), end=self.body.get_corner(UP) + np.array([
+            self.arm_length * np.cos((-1 / 2) * np.pi),
+            self.arm_length * np.sin((-1 / 2) * np.pi),
+            0
+        ]))
 
 
     def turn_right(self):
@@ -137,32 +181,40 @@ class Man(object):
             obj.move_to(obj.get_center() + DOWN * drop[1])
 
     def walk_left(self):
-        self.left_pupil.move_to(self.left_eye.get_center() + LEFT * 0.05 * self.ratio)
-        self.right_pupil.move_to(self.right_eye.get_center() + LEFT * 0.05 * self.ratio)
+        self.left_pupil.move_to(self.left_eye_ball.get_center() + LEFT * 0.05 * self.ratio)
+        self.right_pupil.move_to(self.right_eye_ball.get_center() + LEFT * 0.05 * self.ratio)
         self.me.add_updater(self.body_walk_move_left)
         self.left_leg.walk_left()
         self.right_leg.walk_left()
+        self.left_arm.start_walk()
+        self.right_arm.start_walk()
 
     def stop_walk_left(self):
-        self.left_pupil.move_to(self.left_eye.get_center())
-        self.right_pupil.move_to(self.right_eye.get_center())
+        self.left_pupil.move_to(self.left_eye_ball.get_center())
+        self.right_pupil.move_to(self.right_eye_ball.get_center())
         self.me.remove_updater(self.body_walk_move_left)
         self.left_leg.stop_walk_left()
         self.right_leg.stop_walk_left()
+        self.left_arm.stop_walk()
+        self.right_arm.stop_walk()
 
     def walk_right(self):
-        self.left_pupil.move_to(self.left_eye.get_center() + RIGHT * 0.05 * self.ratio)
-        self.right_pupil.move_to(self.right_eye.get_center() + RIGHT * 0.05 * self.ratio)
+        self.left_pupil.move_to(self.left_eye_ball.get_center() + RIGHT * 0.05 * self.ratio)
+        self.right_pupil.move_to(self.right_eye_ball.get_center() + RIGHT * 0.05 * self.ratio)
         self.me.add_updater(self.body_walk_move_right)
         self.left_leg.walk_right()
         self.right_leg.walk_right()
+        self.left_arm.start_walk()
+        self.right_arm.start_walk()
 
     def stop_walk_right(self):
-        self.left_pupil.move_to(self.left_eye.get_center())
-        self.right_pupil.move_to(self.right_eye.get_center())
+        self.left_pupil.move_to(self.left_eye_ball.get_center())
+        self.right_pupil.move_to(self.right_eye_ball.get_center())
         self.me.remove_updater(self.body_walk_move_right)
         self.left_leg.stop_walk_right()
         self.right_leg.stop_walk_right()
+        self.left_arm.stop_walk()
+        self.right_arm.stop_walk()
 
     def stand_straight(self):
         self.left_leg.stand_straight()
--- test.py
+++ test.py
@@ -6,6 +6,8 @@ from physic.space import PhysicalSpace
 
 class TestMan(MovingCameraScene):
     def construct(self):
+
+        # self.camera.background_color = GREEN
         space = PhysicalSpace(self, 1 / config.frame_rate)
         self.add(space)
         ground = PhysicalGround(y_pos=-2, start=LEFT * 10, end=RIGHT * 10)
@@ -20,8 +22,6 @@ class TestMan(MovingCameraScene):
         main_role = Man(ground.line.get_bottom() -1 , 0.5)
         space.add_body_and_shape(*main_role.bodies)
 
-
-
         body = main_role.it()
         self.play(FadeIn(body))
 
@@ -31,33 +31,14 @@ class TestMan(MovingCameraScene):
         self.wait(2)
 
         main_role.walk_left()
-        self.wait(5)
-
-        main_role.stop_walk_left()
         self.wait(2)
 
-class DrawLineToCircleEdge(Scene):
-    def construct(self):
-        # Step 1: 创建一个圆形
-        radius = 2
-        circle = Circle(radius=radius, color=BLUE)
-        self.add(circle)
-
-        # Step 2: 选择圆内的一个随机起始点
-        # 这里我们选择一个相对于圆心的固定点来做示例
-        start_point = circle.get_center() + DOWN * 1  # 在圆内的一个点
-
-        # Step 3: 计算从起始点到圆边界的交点
-        # 方向向量是从圆心到起始点的方向
-        x0, y0, z0 = start_point
-        end_x = np.sqrt(radius ** 2 - y0 ** 2)  # 通过圆方程计算在 y0 高度下的边界点的 x 坐标
-        end_point = np.array([end_x, y0, 0])
-        # Step 4: 创建一条从起点到圆边缘的直线
-        line = Line(start=start_point, end=end_point, color=RED)
-
-        # 将起点和直线添加到场景中
-        dot = Dot(start_point, color=YELLOW)  # 标记起始点
-        self.add(dot, line)
-
-        # 停留一段时间以便观察效果
+        main_role.stop_walk_left()
+        main_role.stand_straight()
+        self.wait(2)
+        main_role.face_front(self)
+        self.wait(2)
+        main_role.face_back(self)
+        self.wait(2)
+        main_role.face_front(self)
         self.wait(2)
